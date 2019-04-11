edafab340faf5cb7f2fc2007fcd9500081f98cad
janEbert <jebert@techfak.uni-bielefeld.de>
2019-04-11 18:30:59 +0200

    Add notebook to source code generation

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ### Try out, analyze and generate your generators in PyTorch!
 
-An easily usable, performant and extennsible interface for all your GAN needs.
+An easily usable, performant and extensible interface for all your GAN needs.
 
 Training [Generative Adversarial Networks](https://arxiv.org/abs/1406.2661)
 (GANs) is a tiresome and hard task. This is primarily because GAN training has
 not been well understood yet even after a lot of research went into creating,
 understanding and implementing new methods for regularizing and improving GAN
-training (such as [virtual batch 
-normalization](https://arxiv.org/abs/1606.03498), [spectral 
+training (such as [virtual batch
+normalization](https://arxiv.org/abs/1606.03498), [spectral
 normalization](https://arxiv.org/abs/1802.05957) or
 [boundary equilibria](https://arxiv.org/abs/1703.10717)).
 
@@ -17,8 +17,9 @@ In an effort to improve and accelerate research in GAN training, this
 repository collects state of the art approaches and best practices in a modular
 fashion to enable users to quickly switch between and try out different methods
 to find out what works best for their current situation. For that, I provide a
-Jupyter Notebook. A pure Python version is planned which may be more 
-comfortable depending on your use case.
+Jupyter Notebook. A pure Python version can be generated from the notebook
+where parameters are supplied through command line arguments making automated
+experiments much easier.
 
 ## Current Features
 
@@ -26,10 +27,11 @@ Supported framework:
 - PyTorch
 
 Other:
-- True and tested normalizations for GANs (including a [virtual batch 
+- True and tested normalizations for GANs (including a [virtual batch
   normalization](https://arxiv.org/abs/1606.03498) layer)
 - Experimental setup for testing and saving many different parameters
 - Many [GAN hacks](https://github.com/soumith/ganhacks)
+- Generate Python source code from the notebook
 
 This repository offers working multiprocessing on multiple GPUs and CPUs, a
 collection of smart parameters that are documented and easily usable. In the
@@ -41,50 +43,77 @@ box.
 You will need [NumPy](https://www.numpy.org/),
 [Matplotlib](https://matplotlib.org/) and [PyTorch](https://pytorch.org/)
 (including Torchvision). If you want to use the [Jupyter](https://jupyter.org/)
-notebook, install that as well (mandatory at the moment as we only have the
-notebook).
+notebook, install that as well (otherwise generate the source code as described
+below).
+
 To save animations of your GAN training, you may want either
 [FFmpeg](https://ffmpeg.org/) (mp4) or [Pillow](https://python-pillow.org/)
 (gif).
 
+Using [Anaconda](https://www.anaconda.com/), everything except for PyTorch and
+FFmpeg can be installed in one line:
+```zsh
+    conda install numpy matplotlib jupyter pillow
+```
+
 I do not offer installation instructions or a setup file due to the differences
-between CPU and GPU packages. Please look that stuff up yourself in the
-following links.
+between CPU and GPU packages and CUDA versions for PyTorch.
+Please look that stuff up yourself in the following links.
 
 Installation instructions for:
 - [NumPy](https://www.scipy.org/install.html)
 - [Matplotlib](https://matplotlib.org/users/installing.html)
 - [PyTorch](https://pytorch.org/get-started/locally/)
 - [Jupyter](https://jupyter.org/install)
+- [Pillow](https://pillow.readthedocs.io/en/stable/installation.html)
+
+## Generating source code
+
+You can generate source code from the Jupyter notebook. Any parameter will be
+passable as command line arguments, where the default values are the ones
+currently in the notebook.
+
+For the parameters, there are some rules you need to follow to generate source
+code. These are listed in `src/ipynb_to_py.py`.
+
+To generate, simply execute the following:
+```zsh
+    ./src/ipynb_to_py.py
+```
+
+## Experimental dataset
+
+The system was set up using
+[CelebA](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html), but for the
+experiments, [FFHQ](https://github.com/NVlabs/ffhq-dataset) will be used.
 
 ### Coding style
 
 At the moment, in the notebook, I do not adhere to the PEP 8 line size
 guideline. I feel that notebooks are in most cases supposed to be in fullscreen
-and readability would suffer, especially concerning documentation — sorry! If
+and readability would suffer, especially concerning documentation. Sorry! If
 there are enough complaints or I change my mind, I will modify the code
 accordingly.
 
-Any pure Python code later will be documented in ReStructuredText following PEP
-257 and I will limit code and comments to a certain line size.
+Sorry for not enough documentation, that will change with time.
 
 ## TODOs
 
-Methods to implement (still growing):
+Methods to implement (always growing):
 - feature matching
 - minibatch discrimination
 - historical averaging
-- one sided label smoothing
 - bounded equilibrium GANs
 - some missing stuff from [ganhacks](https://github.com/soumith/ganhacks)
 
 Evaluation methods:
-- inception score
 - semi-supervised learning
 
 Other:
+- more documentation of methods
+- automatic experiments for parameter combinations
 - convolutional layer visualization
-- distributed computing version (Horovod?)
+- distributed computing version
 - grid search supporting cloud computing providers
 - more datasets
 - data other than images (sound, 3D objects, ...)
@@ -115,4 +144,7 @@ This is a collection of the papers used for this project.
 
 ## Other resources
 
+- [PyTorch DCGAN
+  Tutorial](https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html)
+  (which a lot is based on)
 - [ganhacks](https://github.com/soumith/ganhacks)
---
+++ src/ipynb_to_py.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import re
+import time
+
+# Relative path from this file to the notebook from which to generate.
+IPYNB_PATH = os.path.join('..', 'notebooks', 'GANerator.ipynb')
+# Relative path from this file to the source file to generate.
+PY_PATH    = 'GANerator_generated.py'
+
+# For this to work, all continuing plot lines after `plt.show` or
+# `plt.imshow` should be marked with  a '# show' comment at the end of
+# the line.
+# This means any of those function spanning more than one line.
+SHOW_PLOTS = True
+
+EMPTY_RE = re.compile(r'^\s*$')
+PLOT_RE  = re.compile(r'^\s*[(plt\.(im)?show\()#\s*show$]')
+
+FORMAT_RULES = '\n'.join(
+    '- Document *every* parameter.',
+    '- Use two spaces before the `#` sign if not at ' \
+            'start of line (ignoring indentation).',
+    '- Use one space after the `#` sign.',
+    "- Space after `'param':`",
+    '- If code spans more than one line, place the ' \
+            'comment on comment-only lines _above_ the ' \
+            'code.',
+    '- After a section (marked by multiple `=` signs), ' \
+            'leave an empty line if the next line is a ' \
+            'comment referring code below (see above ' \
+            'rule).',
+    '- If code is on multiple lines, indent the ' \
+            'continuation more.',
+    # TODO fix below using indentation length
+    '- Currently, no comments or empty lines in '
+            'multi line code are supported.',
+)
+
+
+def filter_nb_only(line):
+    return not (line.startswith('%matplotlib')
+                or line.startswith('from IPython'))
+
+
+def rstrip_spaces(line):
+    i = len(line)
+    while line[i - 1] == '\n':
+        i -= 1
+    return line[:i].rstrip() + line[i:]
+
+
+def indent_content(line):
+    if not SHOW_PLOTS and PLOT_RE.match(line):
+        return '    # ' + rstrip_spaces(line)
+    elif not EMPTY_RE.match(line):
+        return '    ' + rstrip_spaces(line)
+    else:
+        return '\n'
+
+
+def write_src(lines, src_file):
+    """Write lines with four spaces indentation."""
+    if type(lines) is not str:
+        src_file.writelines(map(indent_content, lines))
+    else:
+        src_file.write(indent_content(lines))
+
+
+def write_src_comments(comment_buffer, src_file):
+    if comment_buffer:
+        write_src(map(lambda line: '# ' + line + '\n',
+                      comment_buffer), src_file)
+        comment_buffer.clear()
+
+
+def finish_src_code(code, src_file):
+    if code:
+        # for the last line in the code buffer,
+        # remove everything starting from the last comma
+        comma_ix = code[-1].rfind(',')
+        # add closing bracket for `parser.add_argument`
+        code[-1] = code[-1][:comma_ix] + ')\n'
+        src_file.write(''.join(code))
+        code.clear()
+
+
+def process_parameters(source, src_file):
+    i = 0
+    while not source[i].startswith('params ='):
+        i += 1
+    write_src(source[:i], src_file)
+    write_src('parser = argparse.ArgumentParser()\n', src_file)
+
+    comment_re              = re.compile(r'^\s*# (.*)')
+    code_with_comment_re    = re.compile(r"^\s*'(\S+?.*?)':\s+(.*),  # (.*)")
+    code_without_comment_re = re.compile(r"^\s*'(\S+?.*?)':\s+(.*)")
+    code_without_colon_re  = re.compile(r'^\s*(\S+.*)')
+
+    comment_buffer = []
+    code_buffer    = []
+    for line in source[i + 1:-1]:  # all lines in the params dictionary
+
+        m = comment_re.match(line)
+        if m:
+            finish_src_code(code_buffer, src_file)
+
+            comment_buffer.append(m.groups()[0])
+            continue
+
+        if EMPTY_RE.match(line):
+            finish_src_code(code_buffer, src_file)
+            write_src_comments(comment_buffer, src_file)
+
+            src_file.write('\n')
+            continue
+
+        m = code_with_comment_re.match(line)
+        if m:
+            finish_src_code(code_buffer, src_file)
+            write_src_comments(comment_buffer, src_file)
+
+            groups = m.groups()
+            write_src(map(lambda line: line + '\n', (
+                "parser.add_argument('--{}', nargs='+',".format(groups[0]),
+                '        help="{}",'.format(groups[2]),
+                "        default={})".format(groups[1])
+            )), src_file)
+            continue
+
+        m = code_without_comment_re.match(line)
+        if m and not code_buffer:
+            # enter possible multi line
+            groups = m.groups()
+            write_src((
+                "parser.add_argument('--{}', nargs='+',\n".format(groups[0]),
+                '        help="{}",\n'.format(' '.join(comment_buffer)),
+                '        default='
+            ), src_file)
+            comment_buffer.clear()
+            code_buffer.append(groups[1])
+            continue
+
+        m = code_without_colon_re.match(line)
+        if m and code_buffer:
+            code_buffer.append(m.groups()[0])
+            continue
+
+        else:
+            print('\n'.join((
+                    'The parameters are not well formatted.',
+                    '',
+                    FORMAT_RULES
+            )))
+    finish_src_code(code_buffer, src_file)
+    write_src('params = vars(parser.parse_args())', src_file)
+
+
+def main():
+    with open(os.path.join(os.path.dirname(__file__), IPYNB_PATH), 'r') as f:
+        cells = json.load(f)['cells']
+
+    next_cell_params = False
+    with open(os.path.join(os.path.dirname(__file__), PY_PATH), 'w') as src_file:
+        src_file.writelines(map(lambda line: line + '\n', (
+            '#!/usr/bin/env python3',
+            '',
+            '# This source file was generated by ipynb_to_nb.py,',
+            '# located at GANerator/src/ipynb_to_nb.py.',
+            '# Time of creation: ' + time.strftime('%Y-%m-%d %H:%M:%S'),
+            '',
+        )))
+        src_file.write('import argparse')
+        src_file.writelines(filter(filter_nb_only, cells[0]['source']))
+        src_file.write('\n\ndef main():')
+        for cell in cells[1:-1]:
+            src_file.write('\n')
+            source = cell['source']
+            # TODO better use metadata for finding parameter cell
+            if len(source) == 1 and source[0] == '# Parameters':
+                next_cell_params = True
+            elif next_cell_params:
+                process_parameters(source, src_file)
+                next_cell_params = False
+                src_file.write('\n\n')
+                continue
+            write_src(source, src_file)
+            src_file.write('\n\n')
+        src_file.writelines(map(lambda line: line + '\n', (
+            "\nif __name__ == '__main__':",
+            '    main()\n'
+        )))
+
+
+if __name__ == '__main__':
+    main()
