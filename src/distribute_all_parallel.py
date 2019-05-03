6089a9724141fb3664ddaafe162ddf792610a208
janEbert <janpublicebert@posteo.net>
2019-05-03 23:52:13 +0200

    Rework distribute_all_parallel to use distribute

--- src/distribute_all_parallel.py
+++ src/distribute_all_parallel.py
@@ -3,82 +3,11 @@ import subprocess
 import threading
 import time
 
+from distribute import start_cloud_instance, start_cloud_init, \
+                       start_cloud_process, start_cloud_finish, \
+                       stop_cloud_instance, start_experiment
 import machine
 
-STARTUP_SCRIPT_PATH = os.path.join(os.path.dirname(__file__),
-        '../GANerator_GCP_startup.sh')
-
-
-def start_cloud_instance(num, debug):
-    command = machine.START_COMMAND.format(num)
-    if debug:
-        print(command)
-    else:
-        command = command.split(' ')
-        for i in range(len(command) - 1, -1, -1):
-            part = command[i]
-            if 'GANERATOR_STARTUP' in part:
-                part.replace('GANERATOR_STARTUP', STARTUP_SCRIPT_PATH)
-                command[i] = part
-                break
-
-        subprocess.run(command, check=True)
-
-
-def start_cloud_init(num, debug):
-    if machine.INIT_COMMAND:
-        command = machine.REMOTE_PROCESS_COMMAND.format(
-                command=machine.INIT_COMMAND, suffix=num)
-        if debug:
-            time.sleep(1)
-            print(command)
-        else:
-            subprocess.run(command.split(' '), check=True)
-
-
-def start_cloud_process(command, num, debug):
-    command = machine.REMOTE_PROCESS_COMMAND.format(
-            command=command, suffix=num)
-    if debug:
-        time.sleep(1)
-        print(command)
-    else:
-        subprocess.run(command.split(' '), check=True)
-
-
-def start_cloud_finish(num, debug):
-    if machine.FINISH_COMMAND:
-        command = machine.REMOTE_PROCESS_COMMAND.format(
-                command=machine.FINISH_COMMAND, suffix=num)
-        if debug:
-            time.sleep(1)
-            print(command)
-        else:
-            subprocess.run(command.split(' '), check=True)
-
-
-def stop_cloud_instance(num, debug):
-    command = machine.END_COMMAND.format(num)
-    if debug:
-        time.sleep(1)
-        print(command)
-        time.sleep(1)
-    else:
-        subprocess.run(command.split(' '), check=True)
-
-
-def start_experiment(command, num, debug):
-    if not debug:
-        print('Starting on machine {}-{}'.format(
-                machine.INSTANCE_NAME_PREFIX, num))
-    start_cloud_instance(num, debug)
-    start_cloud_init(num, debug)
-    start_cloud_process(command, num, debug)
-    start_cloud_finish(num, debug)
-    stop_cloud_instance(num, debug)
-    if not debug:
-        print('Experiment on machine {}-{} finished'.format(
-                machine.INSTANCE_NAME_PREFIX, num))
 
 def start_distributed(command, num, debug):
     thread = threading.Thread(target=start_experiment, args=(command, num, debug))
