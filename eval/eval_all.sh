4f8960a6ea54691e1f15317ae1851d68427e2e45
janEbert <janpublicebert@posteo.net>
2019-05-10 22:32:29 +0200

    Improve eval script
    ... by now searching for the latest checkpoint automatically and only
    extracting with tar once.

--- eval/eval_all.sh
+++ eval/eval_all.sh
@@ -8,13 +8,11 @@ mkdir pics
 
 for file in $(ls); do
     intar=$(tar -tf $file)
-    # this was very lazy, we can sort and get the name then which is
-    # much more robust
-    latestintar=$(echo "$intar" | grep '8206_steps\.tar$')
+    lateststeps=$(echo "$intar" | grep -o -P '\d*(?=steps\.tar)' | sort -n)
+    latestintar=$(echo "$intar" | grep $lateststeps'_steps\.tar$')
     paramsintar=$(echo "$intar" | grep '\.pt$')
-    # Both of these create the directory 'GANerator_experiments'
-    tar -xzf $file $latestintar
-    tar -xzf $file $paramsintar
+    # This creates the directory 'GANerator_experiments'
+    tar -xzf $file $latestintar $paramsintar
     python $genpypath --models_file $latestintar --params_file $paramsintar --save_dir pics
     python $fidscorepath $npzpath pics -c 0 --name $file.txt
     gsutil cp $file.txt gs://ganerator/results/
